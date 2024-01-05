6de642a91ae798c7b72356fcf457932255063859
Ben_Lipsky <Benlipsky@icloud.com>
2024-01-05 14:26:05 -0500

    final2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let place = prompt('Enter a place:')
 let vehicle = prompt('Enter a vehicle, and add an "s" after:')
-let noun = prompt('Enter a noun:')
+let noun = prompt('Enter a proper noun:')
 let animals = prompt('Enter a animal, and add an "s" after:')
 let verb = prompt('Enter a verb:')
 let adverb = prompt('Enter an adverb:')
@@ -14,11 +14,11 @@ let reaction = prompt('Enter an adverb ending in "ly":')
 
 
 
-let story = `When the day began, the ${place} became full of life. 2 ${vehicle} crashed into the
-side of a ${noun} hospital. 5 ${animals} escaped from a truck that ${verb} away from the crash, and 
-they began to jump from car to car very ${adverb}. 3 houses then simultaneously caught fire due to 
-a ${gas} leak caused by 8 ${worker} on strike, while 9 planes all crashed over the town of ${town}
-causing debris to rain from above. Only 4 ${athlete} were injured, ${reaction}.`
+let story = `When the day began, the Jungle, which happened to have a ${place} in it, became full of life. 2 ${vehicle} crashed off of the highway 
+into the side of a ${noun} cave. 5 ${animals} escaped from a truck that ${verb} away from the crash, and 
+they began to jump from tree to tree very ${adverb}. 3 wood cabins then simultaneously caught fire due to 
+a ${gas} leak caused by 8 ${worker} on strike in a plant nearby, while 9 planes all crashed over the town of ${town}
+causing debris to rain on the jungle from above. Only 4 ${athlete} were injured, ${reaction}.`
 
 
 
